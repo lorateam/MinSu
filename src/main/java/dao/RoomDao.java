446d8aa5a39c9415ebd961af905feee73d4e9575
chanchann <www.769440972@qq.com>
2018-05-17 02:34:36 +0800

    fix getTotal()

--- src/main/java/dao/RoomDao.java
+++ src/main/java/dao/RoomDao.java
@@ -7,12 +7,12 @@ import java.sql.*;
 
 public class RoomDao
 {
-    public int getTotal(int id)
+    public int getTotal()
     {
         int total=0;
         try(Connection c=DBUtil.getConnection(); Statement s=c.createStatement();)
         {
-            String sql="select count(*) from room where id="+id;
+            String sql="select count(*) from room";
 
             ResultSet rs=s.executeQuery(sql);
             while(rs.next())
--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,6 +1,15 @@
 package dao;
 
+import util.DBUtil;
+
+import java.sql.Connection;
+
 public class UserDao
 {
-    
+    public int getTotal()
+    {
+        int total=0;
+        try(Connection c= DBUtil.getConnection();)
+
+    }
 }
