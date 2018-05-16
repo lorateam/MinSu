6c8eaaefbf1b1ba1548bc76c2744abbe4b54e3e1
geekhch <709531006@qq.com>
2018-05-17 04:00:20 +0800

    servlet

--- src/main/java/controller/Gotrip.java
+++ src/main/java/controller/Gotrip.java
@@ -1,7 +1,10 @@
 package controller;
+import org.json.JSONObject;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 
 public class Gotrip extends HttpServlet{
     public void doGet(HttpServletRequest request, HttpServletResponse response){
@@ -10,6 +13,16 @@ public class Gotrip extends HttpServlet{
 
     public void doPost(HttpServletResponse response, HttpServletRequest request){
         String actionName = (String)request.getAttribute("action");
-        Execute executor = (String)
+        Execute executor = new Execute();
+        try
+        {
+            PrintWriter out = response.getWriter();
+            JSONObject json = executor.execute(actionName,request);
+            out.print(json);
+            out.flush();
+            out.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
