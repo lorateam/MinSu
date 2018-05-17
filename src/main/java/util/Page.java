ee46a947dc8acea2e06a7e8c57d119439fb89564
chanchann <www.769440972@qq.com>
2018-05-18 01:28:21 +0800

    完成翻页类PageUtil

--- src/main/java/util/Page.java
+++ src/main/java/util/Page.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package util;
 
 public class Page
@@ -7,6 +6,13 @@ public class Page
     int count;
     int total;
     String param;
+
+    public Page(int start,int count)
+    {
+        super();
+        this.start=start;
+        this.count=count;
+    }
 
     public int getStart()
     {
@@ -32,14 +38,18 @@ public class Page
     {
         this.total=total;
     }
-    public Page(int start,int count)
-    {
-        super();
-        this.start=start;
-        this.count=count;
+
+    public String getParam() {
+        return param;
     }
+
+    public void setParam(String param) {
+        this.param = param;
+    }
+
     public boolean isHasPrevious()
     {
+
         if(start==0)
             return false;
         return true;
@@ -62,66 +72,19 @@ public class Page
         }
         return true;
     }
+    public int getTotalPage()
+    {
+        int totalPage;
+        if(0==total%count)
+            totalPage=total/count;
+        else
+            totalPage=total/count+1;
+        if(0==totalPage)
+            totalPage=1;
+        return totalPage;
+    }
+
 
 
 }
-=======
-//package util;
-//
-//public class Page
-//{
-//    int start;
-//    int count;
-//    int total;
-//    String param;
-//
-//    public int getStart()
-//    {
-//        return start;
-//    }
-//    public void setStart(int start)
-//    {
-//        this.start=start;
-//    }
-//    public int getCount()
-//    {
-//        return count;
-//    }
-//    public void setCount(int count)
-//    {
-//        this.count=count;
-//    }
-//    public int getTotal()
-//    {
-//        return total;
-//    }
-//    public void setTotal(int total)
-//    {
-//        this.total=total;
-//    }
-//    public Page(int start,int count)
-//    {
-//        super();
-//        this.start=start;
-//        this.count=count;
-//    }
-//    public boolean isHasPrevious()
-//    {
-//        if(start==0)
-//            return false;
-//        return true;
-//    }
-//    public int getLast()
-//    {
-//        int last;
-//
-//    }
-//    public boolean isHasNext()
-//    {
-//        if(start==getLast())
-//        {
-//
-//        }
-//    }
-//}
->>>>>>> 5248630f2bcba3fb6b72326a81e0617cbc5aa28b
+
