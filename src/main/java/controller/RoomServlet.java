0d89d33fb682dabc3122f54d51faa1e6c28bf5f4
chanchann <www.769440972@qq.com>
2018-05-18 03:33:38 +0800

    roomDao新增分页操作

--- src/main/java/controller/RoomServlet.java
+++ src/main/java/controller/RoomServlet.java
@@ -1,11 +1,20 @@
 package controller;
 
+import dao.RoomDao;
+import jdk.internal.util.xml.impl.Input;
 import model.Room;
+import sun.net.httpserver.HttpServerImpl;
+import util.ImageUtil;
 import util.Page;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @javax.servlet.annotation.WebServlet(name = "RoomServlet")
@@ -18,6 +27,101 @@ public class RoomServlet extends BaseBackServlet
         String room_type=params.get("room_type");
         Room r=new Room();
         r.setRoom_type(room_type);
+
+        File imageFolder=new File(request.getSession().getServletContext().getRealPath("img/room"))
+        File file=new File(imageFolder,r.getId()+".jpg");
+        try
+        {
+            if(null!=is&&0!=is.available())
+            {
+                try(FileOutputStream fos=new FileOutputStream(file))
+                {
+                    byte b[]=new byte[2014*1024];
+                    int length=0;
+                    while(-1!=(length=is.read(b)))
+                    {
+                        fos.write(b,0,length);
+                    }
+                    fos.flush();
+                    BufferedImage img= ImageUtil.chageToJpg(file);
+                    ImageIO.write(img,"jpg",file);
+                }
+                catch(Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+        return "@admin_room_list";
+    }
+
+    public String delete(HttpServletRequest request,HttpServletResponse response,Page page)
+    {
+        int id=Integer.parseInt(request.getParameter("id"));
+        RoomDao.delete(id);
+        return "@admin_room_list";
+    }
+
+    public String edit(HttpServletRequest request,HttpServletResponse response,Page page)
+    {
+        int id=Integer.parseInt(request.getParameter("id"));
+        Room r=RoomDao.get(id);
+        request.setAttribute("r",r);
+        return "page/admin/editRoom.jsp"
+    }
+
+    public String update(HttpServletRequest request,HttpServletResponse response,Page page)
+    {
+        Map<String,String> params=new HashMap<>();
+        InputStream is=super.parseUpload(request,params);
+        String room_type=params.get("room_type");
+        int id=Integer.parseInt(params.get("id"));
+
+        Room r=new Room();
+        r.setId(id);
+        r.setRoom_type(room_type);
+        roomDao.update(r);
+
+        File imageFolder=new File(request.getSession().getServletContext().getRealPath("img/room"));
+        File file=new File(imageFolder,r.getId()+".jpg");
+        file.getParentFile().mkdirs();
+
+        try
+        {
+            if(null!=is&&0!=available())
+            {
+                try(FileOutputStream fos=new FileOutputStream(file))
+                {
+                    byte b[]=new byte[2014*1024];
+                    int length=0;
+                    while(-1!=(length=is.read(b)))
+                    {
+                        fos.write(b,0,length);
+                    }
+                    fos.flush();
+                    BufferedImage img=ImageUtil.chageToJpg(file);
+                    ImageIO.write(img,"jpg",file);
+                }
+                catch (Exception E)
+                {
+                    E.printStackTrace();
+                }
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return "@admin_room_list";
+    }
+
+    public String list(HttpServletRequest request,HttpServletResponse response,Page page)
+    {
+        List<Room> rs=roomDao.list(getStart(),page.getCount());
 
     }
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
--- src/main/java/dao/RoomDao.java
+++ src/main/java/dao/RoomDao.java
@@ -4,6 +4,8 @@ import model.Room;
 import util.DBUtil;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RoomDao
 {
@@ -39,7 +41,7 @@ public class RoomDao
             ps.setString(4,bean.getDescription());
             ps.setString(5,bean.getParking_set());
             ps.setString(6,bean.getWifi());
-            ps.setString(9,bean.getRoom_type());
+            ps.setString(7,bean.getRoom_type());
             ps.execute();
 
             ResultSet rs=ps.getGeneratedKeys();
@@ -79,7 +81,7 @@ public class RoomDao
             ps.setString(4,bean.getDescription());
             ps.setString(5,bean.getParking_set());
             ps.setString(6,bean.getWifi());
-            ps.setString(7.bean.getRoom_type());
+            ps.setString(7,bean.getRoom_type());
             ps.setLong(8,bean.getId());
             ps.execute();
         }
@@ -120,5 +122,38 @@ public class RoomDao
             e.printStackTrace();
         }
         return bean;
+    }
+
+    public List<Room> list()
+    {
+        return list(0,short.MAX_VALUE);
+    }
+
+    public List<Room> list(int start,int count)
+    {
+        List<Room> beans=new ArrayList<Room>();
+        String sql="select * from room order by od desc limit ?,?";
+        try(Connection c=DBUtil.getConnection();PreparedStatement ps=c.prepareStatement();)
+        {
+            ps.setInt(1,start);
+            ps.setInt(2,count);
+
+            ResultSet rs=ps.executeQuery();
+
+            while(rs.next())
+            {
+                Room bean=new Room();
+                int id=rs.getInt(1);
+                String room_type=rs.getString(2);
+                bean.setId(id);
+                bean.setRoom_type(room_type);
+                beans.add(bean);
+            }
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+        return beans;
     }
 }
