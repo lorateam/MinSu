623882d0a2b8f0b23bc39a2e0536f70b92febab9
chanchann <www.769440972@qq.com>
2018-05-18 20:03:43 +0800

    merge

--- src/main/java/controller/Gotrip.java
+++ src/main/java/controller/Gotrip.java
@@ -1,41 +1,41 @@
-package controller;
-import org.json.JSONObject;
-
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.PrintWriter;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Map;
-
-public class Gotrip extends HttpServlet{
-    public void doGet(HttpServletRequest request, HttpServletResponse response){
-        doPost(request,response);
-    }
-
-    public void doPost(HttpServletRequest request, HttpServletResponse response){
-        System.out.println("处理post请求");
-        String actionName = request.getParameter("action");
-        //将request数据解析到Map
-        Enumeration enu=request.getParameterNames();
-        Map data = new HashMap();
-        while(enu.hasMoreElements()){
-            String paraName=(String)enu.nextElement();
-            data.put(paraName, request.getParameter(paraName));
-        }
-        try
-        {
-            Execute executor = new Execute();
-            PrintWriter out = response.getWriter();
-            //执行对应action
-            System.out.println(data);
-            JSONObject json = executor.execute(actionName, data);
-            out.print(json);
-            out.flush();
-            out.close();
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-    }
-}
+//package controller;
+//import org.json.JSONObject;
+//
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.PrintWriter;
+//import java.util.Enumeration;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//public class Gotrip extends HttpServlet{
+//    public void doGet(HttpServletRequest request, HttpServletResponse response){
+//        doPost(request,response);
+//    }
+//
+//    public void doPost(HttpServletRequest request, HttpServletResponse response){
+//        System.out.println("处理post请求");
+//        String actionName = request.getParameter("action");
+//        //将request数据解析到Map
+//        Enumeration enu=request.getParameterNames();
+//        Map data = new HashMap();
+//        while(enu.hasMoreElements()){
+//            String paraName=(String)enu.nextElement();
+//            data.put(paraName, request.getParameter(paraName));
+//        }
+//        try
+//        {
+//            Execute executor = new Execute();
+//            PrintWriter out = response.getWriter();
+//            //执行对应action
+//            System.out.println(data);
+//            JSONObject json = executor.execute(actionName, data);
+//            out.print(json);
+//            out.flush();
+//            out.close();
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+//    }
+//}
--- src/main/java/dao/OrderDao.java
+++ src/main/java/dao/OrderDao.java
@@ -1,51 +1,48 @@
-package dao;
-import java.sql.Connection;
-import util.DBUtil;
-import java.sql.*;
-import model.Order;
-
-public class OrderDao{
-    private Statement s = DBUtil.getStatement();
-
-    public OrderDao() throws Exception {
-    }
-
-    public OrderDao() throws Exception {
-    }
-
-    public void InsertOrder(Order order) throws Exception{
-        
-        String sql = String.format("INSERT INTO `order`(seller, customer, money, arrive_date, leave_date)  values(%d,%d,%f,'%s','%s');",
-                order.getSeller(),order.getCustomer(),order.getMoney(),order.getArrive_date(),order.getLeave_date());
-        System.out.println(sql);
-        s.execute(sql);
-    }
-
-    public Order getOrder(String column, String value) throws Exception{
-        String sql = String.format("select * from `order` where %s=%s;",column, value);
-        System.out.println(sql);
-        ResultSet rs = s.executeQuery(sql);
-        Order order = new Order();
-        while(rs.next()){
-            order.setId(rs.getLong("id"));
-            order.setSeller(rs.getLong("seller"));
-            order.setCustomer(rs.getInt("customer"));
-            order.setMoney(rs.getDouble("money"));
-            order.setTime(rs.getString("create_time"));
-            order.setStatus_handle(rs.getString("status_handle"));
-            order.setStatus_pay(rs.getString("status_pay"));
-        }
-        return order;
-    }
-
-    public void updateOrder(String arrive_date, String leave_time, double money, long id) throws Exception{
-        String sql = String.format("update order set arrive_date=%s, leave_time=%s, money=%f where id = %d", arrive_date,leave_time,money, id);
-        
-        s.execute(sql);
-    }
-
-    public void deleteOrder(long id) throws Exception {
-        String sql = String.format("delete from order where id = %d", id);
-        s.execute(sql);
-    }
-}
+//package dao;
+//import java.sql.Connection;
+//import util.DBUtil;
+//import java.sql.*;
+//import model.Order;
+//
+//public class OrderDao{
+//    private Statement s = DBUtil.getStatement();
+//
+//    public OrderDao() throws Exception {
+//    }
+//
+//    public void InsertOrder(Order order) throws Exception{
+//
+//        String sql = String.format("INSERT INTO `order`(seller, customer, money, arrive_date, leave_date)  values(%d,%d,%f,'%s','%s');",
+//                order.getSeller(),order.getCustomer(),order.getMoney(),order.getArrive_date(),order.getLeave_date());
+//        System.out.println(sql);
+//        s.execute(sql);
+//    }
+//
+//    public Order getOrder(String column, String value) throws Exception{
+//        String sql = String.format("select * from `order` where %s=%s;",column, value);
+//        System.out.println(sql);
+//        ResultSet rs = s.executeQuery(sql);
+//        Order order = new Order();
+//        while(rs.next()){
+//            order.setId(rs.getLong("id"));
+//            order.setSeller(rs.getLong("seller"));
+//            order.setCustomer(rs.getInt("customer"));
+//            order.setMoney(rs.getDouble("money"));
+//            order.setTime(rs.getString("create_time"));
+//            order.setStatus_handle(rs.getString("status_handle"));
+//            order.setStatus_pay(rs.getString("status_pay"));
+//        }
+//        return order;
+//    }
+//
+//    public void updateOrder(String arrive_date, String leave_time, double money, long id) throws Exception{
+//        String sql = String.format("update order set arrive_date=%s, leave_time=%s, money=%f where id = %d", arrive_date,leave_time,money, id);
+//
+//        s.execute(sql);
+//    }
+//
+//    public void deleteOrder(long id) throws Exception {
+//        String sql = String.format("delete from order where id = %d", id);
+//        s.execute(sql);
+//    }
+//}
